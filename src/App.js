1ef1ae651ce933fb6bee9115ac9a16c33e987e9b
yana-kushmar <yana.puzevich@gmail.com>
2023-04-23 21:53:03 -0400

    users list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [users, setUsers] = React.useState([])
+    const [invites, setInvites] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+    const [searchValue, setSearchValue] = React.useState("")
+
+const [success, setSuccess]=React.useState(false)
+
+
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((res) => res.json())
+                .then(json => {
+                    setUsers(json.data)
+                })
+                .catch((err) => {
+                    console.warn(err)
+                    alert("Ошибка при получении пользователей")
+                }).finally(()=> setIsLoading(false))
+    }, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.currentTarget.value)
+    }
+
+
+    const onClickInvite = ((id) => {
+        if (invites.includes(id)) {
+            setInvites(prev => prev.filter(_id => _id !== id))
+        } else {
+            setInvites(prev => [...prev, id])
+        }
+    })
+
+const onClickSendInvites = () => {
+       setSuccess(true)
+}
+
+
+    return (
+        <div className="App">
+
+            {
+                success
+                    ?  (<Success  count={invites.length}/>)
+                    :  (<Users
+                        items={users}
+                        isLoading={isLoading}
+                        searchValue={searchValue}
+                        onChangeSearchValue={onChangeSearchValue}
+                        invites={invites}
+                        onClickInvite={onClickInvite}
+
+                        onClickSendInvites={onClickSendInvites}
+                    />)
+            }
+
+
+        </div>
+    );
 }
 
 export default App;
